bd14c2fd2039c828d461fad853a2b0cf6292a7cd
Mikael Lund <Mlund@mac.com>
2024-07-27 00:36:14 +0200

    Clang format

--- src/myprog.c
+++ src/myprog.c
@@ -6,11 +6,11 @@ int main()
     byte i;
     fc_init(1, 1, 0, 60, 0);
     fc_bordercolor(6);
-    for (i = 0; i < 60; ++i)
-    {
+    for (i = 0; i < 60; ++i) {
         fc_textcolor(1 + (i % 14));
         fc_gotoxy(i, i);
         fc_printf("Hello world #%d", i);
     }
-    while (1);
+    while (1)
+        ;
 }
